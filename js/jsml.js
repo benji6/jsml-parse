aa35b66d4325cf7dbc2daa71e18731baffb288c8
benji6 <benhall22@hotmail.co.uk>
2015-01-10 13:03:35 +0000

    move jsml object to its own file

---
+++ js/jsml.js
@@ -0,0 +1,24 @@
+var jsonml = [
+{
+  "el": "p",
+  "text": "hello, how r you doing?",
+  "children": [
+{
+  "el": "h6",
+  "text": "child1"
+},
+{
+  "el": "h6",
+  "text": "child2"
+}
+]
+},
+{
+  "el": "h1",
+  "text": "hefsgfing?"
+},
+{
+  "el": "h2",
+  "text": "hello, how are you doing?"
+}
+];
--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-var jsonml = [
-  {
-    "el": "p",
-    "text": "hello, how r you doing?",
-    "children": [
-      {
-        "el": "h6",
-        "text": "child1"
-      },
-      {
-        "el": "h6",
-        "text": "child2"
-      }
-    ]
-  },
-  {
-    "el": "h1",
-    "text": "hefsgfing?"
-  },
-  {
-    "el": "h2",
-    "text": "hello, how are you doing?"
-  }
-];
-
 var arrayWalker = function arrayWalker(fn) {
   return function recurse(arr) {
     var i;
