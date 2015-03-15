d350333cc183cc1e14e0adcf93d82a88693fb400
benji6 <benhall22@hotmail.co.uk>
2015-03-15 21:24:50 +0000

    added callback test

--- spec/specBundle.js
+++ spec/specBundle.js
@@ -172,19 +172,26 @@ describe("jsmlParse called on JSML object with properties as follows:", () => {
       });
     });
   });
-  it("callback: executes a callback on the created DOM element taking the following arguments (domElement, parentNode, count)", () => {
-    var jsml = {
-      tag: "div",
-      children: exampleTags.map((tag) => {
-        return {
-          callback: function (domElement, parentNOde, count) {
-
-          },
-          count: 2,
-          tag
-        };
-      }),
-    };
+  describe("callback:", () => {
+    it("callback: executes a callback on the created DOM element taking the following arguments (domElement, parentNode, count)", () => {
+      var counter = 0;
+      var jsml = {
+        tag: "div",
+        children: exampleTags.map((tag) => {
+          return {
+            tag,
+            count: 4,
+            callback: function (domElement, parentNode, count) {
+              expectDomElementsToBeEquivalent(domElement, document.createElement(exampleTags[Math.floor(counter / 4)]));
+              expectDomElementsToBeEquivalent(parentNode, document.createElement("div"));
+              expect(count).toBe(counter % 4);
+              counter++;
+            }
+          };
+        }),
+      };
+      jsmlParse(jsml);
+    });
   });
 });
 
