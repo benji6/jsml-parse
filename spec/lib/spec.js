15b548ec32bb540d9cafd1f22caf48b8a8c77f9f
benji6 <benhall22@hotmail.co.uk>
2015-03-15 22:16:36 +0000

    basic spec for attributes

--- spec/lib/spec.js
+++ spec/lib/spec.js
@@ -15,8 +15,27 @@ describe("jsmlParse called on JSML object with properties as follows:", () => {
   require("./specs/callback.js");
 
   describe("other valid element attributes", () => {
-    it("works", () => {
+    it("if JSML property is a valid attribute name that attribute is assigned to the created DOM element", () => {
+      var p;
 
+      var attributes = {
+        id: "id",
+        className: "testClass"
+      };
+
+      var jsml = {
+        tag: "div",
+      };
+
+      for (p in attributes) {
+        jsml[p] = attributes[p];
+      }
+
+      var domEl = jsmlParse(jsml);
+
+      for (p in attributes) {
+        expect(domEl[p]).toBe(attributes[p]);
+      }
     });
   });
   describe("called with parent DOM Element", () => {
